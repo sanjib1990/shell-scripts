d07b282aedc6229cfea9d0a100e5d47e20b8b1d8
sanjib1990 <devnath.sanjib@gmail.com>
2017-07-11 14:40:17 +0530

    Minor change

--- pull.sh
+++ pull.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-basePath="."
+BASE_PATH="."
 
 if [ ! -z "$1" -a "$1" != " " ]; then
-	basePath="$1"
+	BASE_PATH="$1"
 fi
 
-for d in $basePath/*/; do
-	echo "\n Processing for $d \n"
-    branch=$(git -C $d symbolic-ref --short HEAD)
+for d in ${BASE_PATH}/*/; do
+	echo "\n Processing for ${d} \n"
+    BRANCH=$(git -C ${d} symbolic-ref --short HEAD)
 	
-	echo "\n $d is in branch $branch \n"
-    git -C $d pull origin $branch
+	echo "\n ${d} is in BRANCH ${BRANCH} \n"
+    git -C ${d} pull origin ${BRANCH}
 
     echo "\n Done.."
 done
