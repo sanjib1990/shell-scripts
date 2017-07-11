6799f3ddae9dd3697585af279f238ec1c05b7c46
sanjib1990 <devnath.sanjib@gmail.com>
2017-07-11 14:41:45 +0530

    ignore

--- pull.sh
+++ pull.sh
@@ -2,14 +2,14 @@
 BASE_PATH="."
 
 if [ ! -z "$1" -a "$1" != " " ]; then
-	BASE_PATH="$1"
+    BASE_PATH="$1"
 fi
 
 for d in ${BASE_PATH}/*/; do
-	echo "\n Processing for ${d} \n"
+    echo "\n Processing for ${d} \n"
     BRANCH=$(git -C ${d} symbolic-ref --short HEAD)
-	
-	echo "\n ${d} is in BRANCH ${BRANCH} \n"
+
+    echo "\n ${d} is in BRANCH ${BRANCH} \n"
     git -C ${d} pull origin ${BRANCH}
 
     echo "\n Done.."
