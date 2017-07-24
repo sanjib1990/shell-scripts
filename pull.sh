e86296350b182a981bf09f2cf7c491906dd58cf9
sanjib1990 <devnath.sanjib@gmail.com>
2017-07-24 15:24:52 +0530

    Fetch

--- pull.sh
+++ pull.sh
@@ -5,13 +5,21 @@ if [ ! -z "$1" -a "$1" != " " ]; then
     BASE_PATH="$1"
 fi
 
+processGit()
+{
+    git -C $2 checkout $1
+    echo "\n $2 is in BRANCH $1"
+    git -C $2 fetch --all --prune
+    git -C $2 pull origin $1
+}
+
 for d in ${BASE_PATH}/*/; do
-    echo "\n Processing for ${d} \n"
+    echo "\n Processing for ${d}"
     BRANCH=$(git -C ${d} symbolic-ref --short HEAD)
-
-    echo "\n ${d} is in BRANCH ${BRANCH} \n"
-    git -C ${d} fetch --all --prune
-    git -C ${d} pull origin ${BRANCH}
-
+    echo "\n Checking out Master branch"
+    processGit master ${d}
+    echo "\n Checking out Current branch"
+    processGit ${BRANCH} ${d}
     echo "\n Done.."
+    echo "\n###########################################"
 done
